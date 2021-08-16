1772f1e3abb730401f4e5606c4bb87e0bf1c900e
vNicin <vinibarrosdalmeida@gmail.com>
2021-08-16 17:25:55 -0300

    Parametrização do número de grilos.

--- objeto3/src/objeto3/Objeto3.java
+++ objeto3/src/objeto3/Objeto3.java
@@ -1,41 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package objeto3;
 
 import java.util.Random;
 
-/**
+/*
  *
  * @author Raphael Mathias
+ * @author Vinicius Barros
  */
 public class Objeto3 {
-        
+
          static Random random = new Random();
          static int linhaChegada = 300;
-          
-    /**
+
+    /*
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+
+        int numeroGrilos = 6;
         System.out.println("A linha de chegada é em " + linhaChegada);
-         Grilo grilo1 = new Grilo(1,linhaChegada);
-         new Thread(grilo1).start();
-         Grilo grilo2 = new Grilo(2, linhaChegada);
-         new Thread(grilo2).start();
-         Grilo grilo3 = new Grilo(3, linhaChegada);
-         new Thread(grilo3).start();
-         
-      
-         
-         
-    
-        
+
+        for(int i = 1; i <= numeroGrilos; i++)
+        {
+            Thread t = new Thread(new Grilo(i,linhaChegada));
+
+
+            t.start();
+        }
+
+
+
+
+
+
+
     }
-    
-  
-          
-    
+
+
+
+
 }
